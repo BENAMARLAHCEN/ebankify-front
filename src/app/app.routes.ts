0eb4d7c860ad33ccf955d672747c4f98c90dcac8
LahcenBenAmar <lahcen.ben3@gmail.com>
2025-01-10 09:26:11 +0100

    Refactor routing structure to use lazy loading for authentication and dashboard features, and implement role-based access control

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,53 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/auth/login/login.component';
-import { RegisterComponent } from './components/auth/register/register.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { AuthGuard } from './guards/auth.guard';
-import { HomeComponent } from './components/pages/home.component';
-import { NoAuthGuard } from './guards/no-auth.guard';
+import { DashboardLayoutComponent } from './shared/components/layout/dashboard-layout.component';
+import { RoleGuard } from './core/guards/role.guard';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent , canActivate: [NoAuthGuard] },
-  { path: 'register', component: RegisterComponent,  canActivate: [NoAuthGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
-  { path: '', component: HomeComponent }
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    },
+    {
+        path: '',
+        component: DashboardLayoutComponent,
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./features/dashboard/dashboard.component')
+                    .then(m => m.DashboardComponent)
+            },
+            {
+                path: 'accounts',
+                loadComponent: () => import('./features/accounts/accounts.component')
+                    .then(m => m.AccountsComponent)
+            },
+
+            {
+                path: 'transactions',
+                loadComponent: () => import('./features/transactions/transactions.component')
+                    .then(m => m.TransactionsComponent)
+            },
+            {
+                path: 'loans',
+                loadComponent: () => import('./features/loans/loans.component')
+                    .then(m => m.LoansComponent)
+            },
+            {
+                path: 'bills',
+                loadComponent: () => import('./features/bills/bills.component')
+                    .then(m => m.BillsComponent)
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./features/profile/profile.component')
+                    .then(m => m.ProfileComponent),
+                canActivate: [AuthGuard] 
+            }
+        ]
+    },
+
+
+
 ];

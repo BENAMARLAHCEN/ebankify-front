e1fa37fc1d77bb316c39cbd7b6f949f6a2f52eef
LahcenBenAmar <lahcen.ben3@gmail.com>
2025-01-23 09:12:15 +0100

    Add NgRx store and SweetAlert2; update package.json and app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { provideStore } from '@ngrx/store';
+import { transactionCartReducer } from './features/transaction-rx/store/transaction-cart.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideStore({
+      transactionCart: transactionCartReducer
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideHttpClient(withInterceptors([AuthInterceptor])),
